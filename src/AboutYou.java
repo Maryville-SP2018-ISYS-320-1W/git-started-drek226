e68e993e6912abf344d04f395ddbe283db6f5cde
Mike Magic <mikemagic@thes-mbp.home>
2018-03-13 21:19:38 -0500

    changed strings

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -4,10 +4,10 @@ public class AboutYou {
 	public static void main(String[] args) {
 		// TODO Modify the values in the String literals in ALL_CAPS below
 		System.out.print("Hello, my name is ");
-		System.out.println("YOUgrthrtrttrR_NAME");
+		System.out.println("Derek Rieger");
 
 		System.out.print("And I'm taking this class because ");
-		System.out.println("BECAUSE_WHY");
+		System.out.println("I love to learn and computers are fun!");
 	}
 
 }
