f827bf3e3c63daa7b320a81ede54f6da929b2cbe
Mike Magic <mikemagic@thes-mbp.home>
2018-03-13 21:10:27 -0500

    I cant edit

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -4,7 +4,7 @@ public class AboutYou {
 	public static void main(String[] args) {
 		// TODO Modify the values in the String literals in ALL_CAPS below
 		System.out.print("Hello, my name is ");
-		System.out.println("YOUR_NAME");
+		System.out.println("YOUgrthrtrttrR_NAME");
 
 		System.out.print("And I'm taking this class because ");
 		System.out.println("BECAUSE_WHY");
